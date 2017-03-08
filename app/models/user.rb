47a9b45920755fd7f5010b2bb692c59b4a707690
Alex Forbes-Reed <github@alx.red>
2017-03-08 16:39:31 +0000

    Added email attribute to User model, set email field from data retrieved from Github

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < BaseModel
 
   attribute :name
 
+  # Unique email attribute for the user
+  attribute :email
+  unique    :email
+
   # Unique identifier for this user, in the form "{provider}|{provider-id}"
   attribute :uid
   index     :uid
